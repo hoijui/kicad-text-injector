fca2d67039711b8f0abcdcfc063253d9b67b97ae
Robin Vobruba <hoijui.quaero@gmail.com>
2024-06-01 12:55:34 +0100

    Covers rust code in README.md when doc-testing

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,10 @@ pub mod replacer;
 
 use git_version::git_version;
 
+// This tests rust code in the README with doc-tests.
+// Though, It will not appear in the generated documentaton.
+#[doc = include_str!("../README.md")]
+#[cfg(doctest)]
+pub struct ReadmeDoctests;
+
 pub const VERSION: &str = git_version!();
